b94a4fc63832421c3204c02a7a347beef3de3b6e
John Oliva <joliva63@gmail.com>
2013-08-06 18:59:55 -0400

    partial success in building responsive portrait/landscape layout

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,25 @@
 function doClick(e) {
-    alert($.label.text);
+    alert(this.text);
+}
+
+
+// Event handler for orientation changes to re-orient primary container view
+Ti.Gesture.addEventListener('orientationchange', function (ev) {
+    if (Ti.Gesture.isLandscape(ev.orientation)) {
+      // Update your UI for landscape orientation
+    	$.primary.layout="horizontal";
+    } else {
+      // Update your UI for portrait orientation
+    	$.primary.layout="vertical";
+    }
+  });
+
+// Initialize layout based on orientation
+
+if (Ti.Gesture.isPortrait() === true) {
+	$.primary.layout="vertical";
+} else {
+	$.primary.layout="horizontal";
 }
 
 $.index.open();
