08df064e803745bd677df6017dc4d39b0f60a3ed
John Oliva <joliva63@gmail.com>
2013-08-07 11:59:19 -0400

    First success at dynamic relayout upon orientation change.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -8,18 +8,33 @@ Ti.Gesture.addEventListener('orientationchange', function (ev) {
     if (Ti.Gesture.isLandscape(ev.orientation)) {
       // Update your UI for landscape orientation
     	$.primary.layout="horizontal";
+    	$.login.height="100%";
+    	$.login.width="50%";
+    	$.signup.height="100%";
+    	$.signup.width="50%";
     } else {
       // Update your UI for portrait orientation
     	$.primary.layout="vertical";
+    	$.login.height="50%";
+    	$.login.width="100%";
+    	$.signup.height="50%";
+    	$.signup.width="100%";
     }
   });
 
 // Initialize layout based on orientation
-
 if (Ti.Gesture.isPortrait() === true) {
 	$.primary.layout="vertical";
+	$.login.height="50%";
+	$.login.width="100%";
+	$.signup.height="50%";
+	$.signup.width="100%";
 } else {
 	$.primary.layout="horizontal";
+	$.login.height="100%";
+	$.login.width="50%";
+	$.signup.height="100%";
+	$.signup.width="50%";
 }
 
 $.index.open();
