516747acd4c4a7006381ca9955f5287dff0069f7
John Oliva <joliva63@gmail.com>
2013-11-21 23:17:44 -0500

    Started converting SQL sync adapter over to using scalability framework.

--- app/lib/alloy/sync/sc-sql.js
+++ app/lib/alloy/sync/sc-sql.js
@@ -1,4 +1,5 @@
-var _ = require('alloy/underscore')._;
+var _ = require('alloy/underscore')._,
+	sqldb = require('framework/kernel/kernelSqlDb').sqldb;
 
 // The database name used when none is specified in the
 // model configuration.
@@ -17,137 +18,6 @@ var cache = {
 	config: {},
 	Model: {}
 };
-
-// The sql-specific migration object, which is the main parameter
-// to the up() and down() migration functions.
-//
-// db            The database handle for migration processing. Do not open
-//               or close this as it is a running transaction that ensures
-//               data integrity during the migration process.
-// dbname        The name of the SQLite database for this model.
-// table         The name of the SQLite table for this model.
-// idAttribute   The unique ID column for this model, which is
-//               mapped back to Backbone.js for its update and
-//               delete operations.
-function Migrator(config, transactionDb) {
-	this.db = transactionDb;
-	this.dbname = config.adapter.db_name;
-	this.table = config.adapter.collection_name;
-	this.idAttribute = config.adapter.idAttribute;
-
-	//TODO: normalize columns at compile time - https://jira.appcelerator.org/browse/ALOY-222
-	this.column = function(name) {
-		// split into parts to keep additional column characteristics like
-		// autoincrement, primary key, etc...
-		var parts = name.split(/\s+/);
-		var type = parts[0];
-		switch(type.toLowerCase()) {
-			case 'string':
-			case 'varchar':
-			case 'date':
-			case 'datetime':
-				Ti.API.warn('"' + type + '" is not a valid sqlite field, using TEXT instead');
-			case 'text':
-				type = 'TEXT';
-				break;
-			case 'int':
-			case 'tinyint':
-			case 'smallint':
-			case 'bigint':
-			case 'boolean':
-				Ti.API.warn('"' + type + '" is not a valid sqlite field, using INTEGER instead');
-			case 'integer':
-				type = 'INTEGER';
-				break;
-			case 'double':
-			case 'float':
-			case 'decimal':
-			case 'number':
-				Ti.API.warn('"' + name + '" is not a valid sqlite field, using REAL instead');
-			case 'real':
-				type = 'REAL';
-				break;
-			case 'blob':
-				type = 'BLOB';
-				break;
-			case 'null':
-				type = 'NULL';
-				break;
-			default:
-				type = 'TEXT';
-				break;
-		}
-		parts[0] = type;
-		return parts.join(' ');
-	};
-
-	this.createTable = function(config) {
-		// compose the create query
-		var columns = [];
-		var found = false;
-		for (var k in config.columns) {
-			if (k === this.idAttribute) { found = true; }
-			columns.push(k + " " + this.column(config.columns[k]));
-		}
-
-		// add the id field if it wasn't specified
-		if (!found && this.idAttribute === ALLOY_ID_DEFAULT) {
-			columns.push(ALLOY_ID_DEFAULT + ' TEXT UNIQUE');
-		}
-		var sql = 'CREATE TABLE IF NOT EXISTS ' + this.table + ' ( ' + columns.join(',') + ')';
-
-		// execute the create
-		this.db.execute(sql);
-	};
-
-	this.dropTable = function() {
-		this.db.execute('DROP TABLE IF EXISTS ' + this.table);
-	};
-
-	this.insertRow = function(columnValues) {
-		var columns = [];
-		var values = [];
-		var qs = [];
-
-		// get arrays of column names, values, and value placeholders
-		var found = false;
-		for (var key in columnValues) {
-			if (key === this.idAttribute) { found = true; }
-			columns.push(key);
-			values.push(columnValues[key]);
-			qs.push('?');
-		}
-
-		// add the id field if it wasn't specified
-		if (!found && this.idAttribute === ALLOY_ID_DEFAULT) {
-			columns.push(this.idAttribute);
-			values.push(guid());
-			qs.push('?');
-		}
-
-		// construct and execute the query
-		this.db.execute('INSERT INTO ' + this.table + ' (' + columns.join(',') + ') VALUES (' + qs.join(',') + ');', values);
-	};
-
-	this.deleteRow = function(columns) {
-		var sql = 'DELETE FROM ' + this.table;
-		var keys = _.keys(columns);
-		var len = keys.length;
-		var conditions = [];
-		var values = [];
-
-		// construct the where clause, if necessary
-		if (len) { sql += ' WHERE '; }
-		for (var i = 0; i < len; i++) {
-			conditions.push(keys[i] + ' = ?');
-			values.push(columns[keys[i]]);
-		}
-		sql += conditions.join(' AND ');
-
-		// execute the delete
-		this.db.execute(sql, values);
-	};
-}
 
 function Sync(method, model, opts) {
 	var table =  model.config.adapter.collection_name,
@@ -288,110 +158,6 @@ function Sync(method, model, opts) {
 
 }
 
-// Gets the current saved migration
-function GetMigrationFor(dbname, table) {
-	var mid = null;
-	var db = Ti.Database.open(dbname);
-	db.execute('CREATE TABLE IF NOT EXISTS migrations (latest TEXT, model TEXT);');
-	var rs = db.execute('SELECT latest FROM migrations where model = ?;', table);
-	if (rs.isValidRow()) {
-		mid = rs.field(0) + '';
-	}
-	rs.close();
-	db.close();
-	return mid;
-}
-
-function Migrate(Model) {
-	// get list of migrations for this model
-	var migrations = Model.migrations || [];
-
-	// get a reference to the last migration
-	var lastMigration = {};
-	if (migrations.length) { migrations[migrations.length-1](lastMigration); }
-
-	// Get config reference
-	var config = Model.prototype.config;
-
-	// Get the db name for this model and set up the sql migration obejct
-	config.adapter.db_name = config.adapter.db_name || ALLOY_DB_DEFAULT;
-	var migrator = new Migrator(config);
-
-	// Get the migration number from the config, or use the number of
-	// the last migration if it's not present. If we still don't have a
-	// migration number after that, that means there are none. There's
-	// no migrations to perform.
-	var targetNumber = typeof config.adapter.migration === 'undefined' ||
-		config.adapter.migration === null ? lastMigration.id : config.adapter.migration;
-	if (typeof targetNumber === 'undefined' || targetNumber === null) {
-		var tmpDb = Ti.Database.open(config.adapter.db_name);
-		migrator.db = tmpDb;
-		migrator.createTable(config);
-		tmpDb.close();
-		return;
-	}
-	targetNumber = targetNumber + ''; // ensure that it's a string
-
-	// Create the migration tracking table if it doesn't already exist.
-	// Get the current saved migration number.
-	var currentNumber = GetMigrationFor(config.adapter.db_name, config.adapter.collection_name);
-
-	// If the current and requested migrations match, the data structures
-	// match and there is no need to run the migrations.
-	var direction;
-	if (currentNumber === targetNumber) {
-		return;
-	} else if (currentNumber && currentNumber > targetNumber) {
-		direction = 0; // rollback
-		migrations.reverse();
-	} else {
-		direction = 1;  // upgrade
-	}
-
-	// open db for our migration transaction
-	db = Ti.Database.open(config.adapter.db_name);
-	migrator.db = db;
-	db.execute('BEGIN;');
-
-	// iterate through all migrations based on the current and requested state,
-	// applying all appropriate migrations, in order, to the database.
-	if (migrations.length) {
-		for (var i = 0; i < migrations.length; i++) {
-			// create the migration context
-			var migration = migrations[i];
-			var context = {};
-			migration(context);
-
-			// if upgrading, skip migrations higher than the target
-			// if rolling back, skip migrations lower than the target
-			if (direction) {
-				if (context.id > targetNumber) { break; }
-				if (context.id <= currentNumber) { continue; }
-			} else {
-				if (context.id <= targetNumber) { break; }
-				if (context.id > currentNumber) { continue; }
-			}
-
-			// execute the appropriate migration function
-			var funcName = direction ? 'up' : 'down';
-			if (_.isFunction(context[funcName])) {
-				context[funcName](migrator);
-			}
-		}
-	} else {
-		migrator.createTable(config);
-	}
-
-	// update the saved migration in the db
-	db.execute('DELETE FROM migrations where model = ?', config.adapter.collection_name);
-	db.execute('INSERT INTO migrations VALUES (?,?)', targetNumber, config.adapter.collection_name);
-
-	// end the migration transaction
-	db.execute('COMMIT;');
-	db.close();
-	migrator.db = null;
-}
-
 function installDatabase(config) {
 	// get the database name from the db file path
 	var dbFile = config.adapter.db_file;
@@ -495,10 +261,9 @@ module.exports.afterModelCreate = function(Model, name) {
 		return cache.Model[name];
 	}
 
-	// create and migrate the Model class
+	// create the Model class
 	Model = Model || {};
 	Model.prototype.idAttribute = Model.prototype.config.adapter.idAttribute;
-	Migrate(Model);
 
 	// Add the Model class to the cache
 	cache.Model[name] = Model;
